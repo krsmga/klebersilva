54336aaf1f01484b15b53e4d999e42aefefdea46
Kleber Silva <krsmga@gmail.com>
2022-08-28 13:33:21 -0300

    Novos materiais no portfólio e sobre mim

--- assets/js/browser-detect.js
+++ assets/js/browser-detect.js
@@ -1,20 +1,28 @@
-var ua = navigator.userAgent.toLowerCase()
-var isSafari = false
-
-try {
-  isSafari =
-    /constructor/i.test(window.HTMLElement) ||
-    (function (p) {
-      return p.toString() === '[object SafariRemoteNotification]'
-    })(!window['safari'] || safari.pushNotification)
-} catch (err) { }
-
-isSafari = isSafari || (ua.indexOf('safari') != -1 && !(ua.indexOf('chrome') != -1) && ua.indexOf('version/') != -1)
-
-if (isSafari) {
-  var element = document.getElementById('video-banner')
-  element.classList.add('hidden')
-} else {
-  var element = document.getElementById('image-banner')
-  element.classList.add('hidden')
+var getBrowser = function () 
+{
+    //isChrome
+    if (window.navigator.userAgent.indexOf("Chrome") > -1) 
+    { 
+        return 'chrome';
+    }
+    else if (window.navigator.userAgent.indexOf("MSIE ") > -1 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
+    { 
+        return 'msie';
+    }
+    else if (window.navigator.userAgent.indexOf("Edge/") > -1)
+    { 
+        return 'edge';
+    }
+    else if (window.navigator.userAgent.indexOf("Safari") > -1 && window.navigator.userAgent.indexOf('Chrome') == -1)
+    { 
+        return 'safari';
+    }
+    else if (window.navigator.userAgent.indexOf("Firefox") > -1)
+    { 
+        return 'firefox';
+    }
+    else if (window.navigator.userAgent.indexOf("Edg/") > -1)
+    { 
+        return 'edge';
+    }
 }
